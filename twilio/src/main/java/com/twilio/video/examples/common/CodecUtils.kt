094ff9e8a369cf65aeb556e922c670bf1d3f9cc1
Griffin Oluoch <griffinoluoch4@gmail.com>
2021-04-15 12:26:04 +0300

    Working twilio integration

---
+++ twilio/src/main/java/com/twilio/video/examples/common/CodecUtils.kt
@@ -0,0 +1,21 @@
+@file:JvmName("CodecUtils")
+package com.twilio.video.examples.common
+
+import tvi.webrtc.HardwareVideoDecoderFactory
+import tvi.webrtc.HardwareVideoEncoderFactory
+
+fun isH264Supported(): Boolean {
+    val hardwareVideoEncoderFactory =
+        HardwareVideoEncoderFactory(null, true, true)
+    val hardwareVideoDecoderFactory =
+        HardwareVideoDecoderFactory(null)
+
+    val h264EncoderSupported = hardwareVideoEncoderFactory.supportedCodecs.any {
+        it.name.equals("h264", ignoreCase = true)
+    }
+    val h264DecoderSupported = hardwareVideoDecoderFactory.supportedCodecs.any {
+        it.name.equals("h264", ignoreCase = true)
+    }
+
+    return h264EncoderSupported && h264DecoderSupported
+}
